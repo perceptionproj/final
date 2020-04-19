f99aa8fdf5b194811a2553c13e24c196111bfabb
Andrea Vallone <vallone.andrea96@gmail.com>
2020-04-19 11:01:16 +0200

    minor changes

--- camera_calibration.py
+++ camera_calibration.py
@@ -1,17 +1,18 @@
+# %% IMPORTS
 import numpy as np
 import cv2
 import glob
-import matplotlib.pyplot as plt
 
-cam = '../Stereo_calibration_images'
-images_left = glob.glob(cam + '/left*.png')
-images_right = glob.glob(cam + '/right*.png')
+# %% LOAD IMAGES
+cam = '../calibration/calibration_patterns'
+images_left = glob.glob(cam + '/left/*.png')
+images_right = glob.glob(cam + '/right/*.png')
 
 assert images_right, images_left
 images_right.sort()
 images_left.sort()
-#print(images)
 
+# %% INITIALIZE OBJECT POINTS
 nb_vertical = 9
 nb_horizontal = 6
 
@@ -26,68 +27,73 @@ imgpoints_l = [] # 2d points in image plane.
 objpoints_r = [] # 3d point in real world space
 imgpoints_r = [] # 2d points in image plane.
 
+# %% EXTRACT CHECKERBOARD CORNERS (AND DISPLAY THEM)
 for fname in images_left:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(gray, patternSize = (nb_vertical, nb_horizontal))
+	
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints_l.append(objp)
         imgpoints_l.append(corners)
-
+		
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners,ret)
-        cv2.imshow('img',img)
+        cv2.imshow('Left Calibration Pattern',img)
         cv2.waitKey(5)
 cv2.destroyAllWindows()
+
 
 for fname in images_right:
     img = cv2.imread(fname)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     ret, corners = cv2.findChessboardCorners(gray, patternSize = (nb_vertical, nb_horizontal))
+	
     # If found, add object points, image points (after refining them)
     if ret == True:
         objpoints_r.append(objp)
         imgpoints_r.append(corners)
-
+		
         # Draw and display the corners
         img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners,ret)
-        cv2.imshow('img',img)
+        cv2.imshow('Right Calibration Pattern',img)
         cv2.waitKey(5)
 cv2.destroyAllWindows()
 
-
+# %% CALIBRATE CAMERA (AND SAVE DISTORTION COEFFICIENTS)
 ret_l, mtx_l, dist_l, rvecs_l, tvecs_l = cv2.calibrateCamera(objpoints_l, imgpoints_l, gray.shape[::-1], None, None, flags = cv2.CALIB_RATIONAL_MODEL)
-img_l = cv2.imread(cam + '/left-0001.png')
+img_l = cv2.imread(images_left[0])
 h,  w = img_l.shape[:2]
 newcameramtx_l, roi_l = cv2.getOptimalNewCameraMatrix(mtx_l,dist_l,(w,h),1,(w,h))
 
 ret_r, mtx_r, dist_r, rvecs_r, tvecs_r = cv2.calibrateCamera(objpoints_r, imgpoints_r, gray.shape[::-1], None, None, flags = cv2.CALIB_RATIONAL_MODEL)
-img_r = cv2.imread(cam + '/right-0001.png')
+img_r = cv2.imread(images_right[0])
 h,  w = img_r.shape[:2]
 newcameramtx_r, roi_r = cv2.getOptimalNewCameraMatrix(mtx_r,dist_r,(w,h),1,(w,h))
 
 # Saving coefficients
-dir = "camera_params/"
+dir = "../calibration/calibration_matrix/"
 np.save(dir + "mtx_l", mtx_l)
 np.save(dir + "dist_l", dist_l)
 np.save(dir + "mtx_r", mtx_r)
 np.save(dir + "dist_r", dist_r)
 
+# %% UNDISTORT, CROP AND SHOW IMAGE
 # undistort
 img_l_undist = cv2.undistort(img_l, mtx_l, dist_l, None, newcameramtx_l)
-img_l = cv2.imread(cam + '/left-0001.png')
+img_l = cv2.imread(images_left[0])
 
-# cv2.imshow('distorted image', img_l)
-# cv2.waitKey()
-
-cv2.imshow('undistorted image', img_l_undist)
+cv2.imshow('Distorted Image', img_l)
+cv2.waitKey()
+cv2.imshow('Undistorted Image', img_l_undist)
 cv2.waitKey()
 
 # crop the image
 x,y,w,h = roi_l
 img_l_undist_crop = img_l_undist[y:y+h, x:x+w]
-# plt.figure(figsize=(10,10))
-cv2.imshow('cropped_undist', img_l_undist_crop)
+
+cv2.imshow('Undistorted and Cropped Image', img_l_undist_crop)
 cv2.waitKey()
+
 cv2.destroyAllWindows()
