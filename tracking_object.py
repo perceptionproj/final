702a396e4e2ad191fea38b63f37d64130e3c7274
CarlosRC <s192340@student.dtu.dk>
2020-04-23 00:27:51 +0200

    tracking_object.py Improved. Using Blackground Subtration

--- tracking_object.py
+++ tracking_object.py
@@ -4,7 +4,7 @@ import glob
 import imutils
 
 #%% LOAD IMAGES
-dataset = '../datasets/conveyor_without_occlusions'
+dataset = '../Final_Project_GitHub/datasets/conveyor_without_occlusions'
 images_left = glob.glob(dataset + '/left/*.png')
 images_right = glob.glob(dataset + '/right/*.png')
 
@@ -16,7 +16,7 @@ images_left.sort()
 
 #%% LOAD MATRICES AND PARAMETERS
 
-dir = "../calibration/calibration_matrix/"
+dir = "../Final_Project_GitHub/calibration/calibration_matrix/"
 mtx_l = np.load(dir + "camera_matrix_l.npy")
 dist_l = np.load(dir + "distortion_coeff_l.npy")
 mtx_l_new = np.load(dir + "camera_matrix_l_new.npy")
@@ -32,50 +32,53 @@ mtx_P_r = np.load(dir + "projection_matrix_r.npy")
 
 #%% MOTION DETECTION AND BACKGROUND MASK
 
-firstFrame = None
-min_area = 4000
+# Create VideoWriter object in oder to record the result
+img_sample = cv2.imread(images_left[0])
+frame_height,  frame_width = img_sample.shape[:2]
+object_detection = cv2.VideoWriter('Object_Detection.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 40, (frame_width,frame_height))
+
+min_area = 3500
+
+#Define kernels
+kernel_ero = np.ones((20,20), np.uint8)
+kernel_dil = np.ones((20,20), np.uint8)
+kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3,3))
+
+#Initialize Blackground Subtractor
+fgbg = cv2.createBackgroundSubtractorMOG2()
 
 for i in range(n_images):
     #grab the current frame 
     frame = cv2.imread(images_left[i])
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (27,27), 0)
 
-    if firstFrame is None:
-        firstFrame = gray
-        continue
-    
-    # compute the absolute difference between the current frame and the first frame
-    # delta = |background_model – current_frame|. In our case, background_model=firstFrame
-    frameDelta = cv2.absdiff(firstFrame, gray)
-    thresh = cv2.threshold(frameDelta, 40, 255, cv2.THRESH_BINARY)[1]
+    # Apply the blackground subtration with the current frame
+    fgmask = fgbg.apply(frame)
+    fgmask = cv2.morphologyEx(fgmask, cv2.MORPH_OPEN, kernel)
 
-    # dilate the thresholded image to fill in holes, the find the contours on the thresholded image
-    thresh = cv2.dilate(thresh, None, iterations=4)
-    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # Erode and dilate the mask
+    erosion = cv2.erode(fgmask, kernel_ero, iterations=2)
+    dilation = cv2.dilate(erosion, kernel_dil, iterations=1)
+
+    # Find the contours
+    cnts = cv2.findContours(fgmask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
-    for c in cnts:
-        # if contour too small, ignore it
-        if cv2.contourArea(c) < min_area:
-            continue
+    if len(cnts)>0:
+        c = max(cnts, key=cv2.contourArea)
+        area = cv2.contourArea(c)
+                
+        if area > min_area:           
+            # compute the bounding box for the contour, draw it on the frame
+            (x,y,w,h) = cv2.boundingRect(c)
+            cv2.rectangle(frame, (int(x-10),int(y-10)), (int(x+w+10), int(y+h+10)), (0,0,255),2)
+                
+    object_detection.write(frame)
+    cv2.namedWindow("Blackground Subtration", cv2.WINDOW_FULLSCREEN)
+    cv2.imshow("Blackground Subtration", frame)
+    cv2.imshow("Dilation", dilation)
 
-        # compute the bounding box for the contour, draw it on the frame
-        (x,y,w,h) = cv2.boundingRect(c)
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0),2)
-
-    cv2.putText(frame, "Object Detection", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-    cv2.putText(frameDelta, "Delta Frame", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-    cv2.putText(thresh, "Thresh", (50,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
-
-    output = np.hstack((frameDelta, thresh))
-    cv2.namedWindow("Motion Detection", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("Motion Detection", 1300, 500)
-    cv2.imshow("Motion Detection",output)
-    cv2.imshow("Object Detection", frame)
-
-	#cv2.imshow("Frame Delta", frameDelta)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+    
+object_detection.release()
 cv2.destroyAllWindows()
